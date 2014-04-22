a48f2008fc01d1e145d89f6ab2f43b5472ab43d8
David <david.ferrero.ferrero@gmail.com>
2014-04-22 11:50:55 +0200

    Add coche bean

--- src/main/java/innova4b/empresaReparto/coche/domain/Coche.java
+++ src/main/java/innova4b/empresaReparto/coche/domain/Coche.java
@@ -9,28 +9,86 @@ import javax.persistence.Table;
 
 import innova4b.empresaReparto.empresa.domain.Empresa;
 
-
 @Entity
-@Table(name="coche")
+@Table(name = "coche")
 public class Coche {
-	
+
 	@Id
 	@GeneratedValue
 	private int id;
 	@ManyToOne
-	@JoinColumn(name="empresa_id", referencedColumnName="id")
+	@JoinColumn(name = "empresa_id", referencedColumnName = "id")
 	private Empresa empresa;
+
+	private int kilometros;
+	private String combustible;
+	private String marca;
+	private String matricula;
+	private String modelo;
+	private boolean disponible;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public Empresa getEmpresa() {
 		return empresa;
 	}
+
 	public void setEmpresa(Empresa empresa) {
 		this.empresa = empresa;
+	}
+
+	public int getKilometros() {
+		return kilometros;
+	}
+
+	public void setKilometros(int kilometros) {
+		this.kilometros = kilometros;
+	}
+
+	public String getCombustible() {
+		return combustible;
+	}
+
+	public void setCombustible(String combustible) {
+		this.combustible = combustible;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
 	}
 
 }
