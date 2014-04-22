db2119e7d8dc8fc7ee4bc04787a9d68bf7dbe123
joseAyudarte91 <jose.ayudarte@gmail.com>
2014-04-22 11:47:24 +0200

    Added CocheController

--- src/main/java/innova4b/empresaReparto/coche/web/CocheController.java
+++ src/main/java/innova4b/empresaReparto/coche/web/CocheController.java
@@ -1,21 +1,12 @@
 package innova4b.empresaReparto.coche.web;
 
-import innova4b.empresaReparto.empresa.domain.Empresa;
-import innova4b.empresaReparto.empresa.repository.EmpresaDao;
-import innova4b.empresaReparto.empresa.service.EmpresaService;
-import innova4b.empresaReparto.exceptions.EmpresaWithCochesException;
-import innova4b.empresaReparto.exceptions.EmpresaWithEmpleadosException;
-
-import javax.validation.Valid;
+import innova4b.empresaReparto.coche.repository.CocheDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequestMapping("/coche")
 @Controller
