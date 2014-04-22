969b7cbae83e4a928c068e56844f7d7ae45d2ab4
ivantxoo <ivantxo@gmail.com>
2014-04-22 12:53:58 +0200

    Actualizacion clase Empleado

--- src/main/java/innova4b/empresaReparto/empleado/domain/Empleado.java
+++ src/main/java/innova4b/empresaReparto/empleado/domain/Empleado.java
@@ -1,5 +1,7 @@
 package innova4b.empresaReparto.empleado.domain;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,6 +22,16 @@ public class Empleado {
 	@JoinColumn(name="empresa_id", referencedColumnName="id")
 	private Empresa empresa;
 	
+	private int activo;
+	private String dni;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private Date fechaNacimiento;
+	private String telefono;
+	private String email;
+	
+	private int jefe;
 	public int getId() {
 		return id;
 	}
@@ -31,6 +43,60 @@ public class Empleado {
 	}
 	public void setEmpresa(Empresa empresa) {
 		this.empresa = empresa;
+	}
+	public int getActivo() {
+		return activo;
+	}
+	public void setActivo(int activo) {
+		this.activo = activo;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getJefe() {
+		return jefe;
+	}
+	public void setJefe(int jefe) {
+		this.jefe = jefe;
 	}
 	
 	
