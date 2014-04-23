926b0ca1fcfc4a480146ab8975a1626842ac4bf5
Endika Esteibar <esteibar@gmail.com>
2014-04-23 13:17:25 +0200

    Listar incidencias y ver cada una

--- src/main/java/innova4b/empresaReparto/incidencia/repository/IncidenciaDao.java
+++ src/main/java/innova4b/empresaReparto/incidencia/repository/IncidenciaDao.java
@@ -20,14 +20,9 @@ public class IncidenciaDao {
 	private SessionFactory sessionFactory;
 	
 	public Incidencia getById(int idIncidencia) throws IndexOutOfBoundsException {
-//		return (Incidencia)sessionFactory.getCurrentSession().createQuery("from Incidencia where id=?").setInteger(0, idIncidencia).list().get(0);
-		return (Incidencia)sessionFactory.getCurrentSession().createQuery("from Incidencia").list().get(0);
+		return (Incidencia)sessionFactory.getCurrentSession().createQuery("from Incidencia where id=?").setInteger(0, idIncidencia).list().get(0);
 
 	} 
-
-	public Incidencia get(int id){
-		return (Incidencia) sessionFactory.getCurrentSession().get(Incidencia.class, id);
-	}
 	
 	public List<Incidencia> list(int idCoche) throws IndexOutOfBoundsException {
 		 
