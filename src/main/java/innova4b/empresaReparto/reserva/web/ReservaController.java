50003a6d21199cacacae767d434b34ca6af1c44f
joseAyudarte91 <jose.ayudarte@gmail.com>
2014-04-23 13:15:06 +0200

    Add addReserva function to ReservaController, fixes needed

--- src/main/java/innova4b/empresaReparto/reserva/web/ReservaController.java
+++ src/main/java/innova4b/empresaReparto/reserva/web/ReservaController.java
@@ -1,9 +1,13 @@
 package innova4b.empresaReparto.reserva.web;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import innova4b.empresaReparto.coche.repository.CocheDao;
 import innova4b.empresaReparto.empleado.repository.EmpleadoDao;
+import innova4b.empresaReparto.empresa.domain.Empresa;
+import innova4b.empresaReparto.exceptions.IncorrectPasswordException;
+import innova4b.empresaReparto.exceptions.UserNotFoundException;
 import innova4b.empresaReparto.login.domain.Usuario;
 import innova4b.empresaReparto.reserva.domain.Reserva;
 import innova4b.empresaReparto.reserva.repository.ReservaDao;
@@ -12,6 +16,7 @@ import innova4b.empresaReparto.reserva.service.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +42,20 @@ public class ReservaController {
 		//Empleado empleado = empleadoDao.getEmpleadoById(usuario.getId());  @FIXME
 		model.addAttribute("reserva", reserva);	
 		return "reserva/new";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addReserva(@Valid Reserva reserva, BindingResult result) {
+		if (result.hasErrors())
+			return "reserva/new";
+		/*try{ @FIXME
+			reservaService.insert(reserva);
+		} catch (CocheNotFreeForReservationException coNotFreeEx) {
+			redirect.addFlashAttribute("error", coNotFreeEx.getMessage());
+		} catch (LastDateNotFutureOfFirstDateException LastDatNotFutOfFirstEx) {
+			redirect.addFlashAttribute("error", LastDatNotFutOfFirstEx.getMessage());
+		}*/
+		return "redirect:/empresaReparto/coche/list";
 	}
 	
 	
