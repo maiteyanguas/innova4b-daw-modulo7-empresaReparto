4b6a713ef66f2c79fc9197c714784a0be35f671d
xhidalgol <xhidalgol@gmail.com>
2014-04-25 11:35:06 +0200

    lista jefes

--- src/main/java/innova4b/empresaReparto/empleado/repository/EmpleadoDao.java
+++ src/main/java/innova4b/empresaReparto/empleado/repository/EmpleadoDao.java
@@ -48,4 +48,7 @@ public class EmpleadoDao {
 	public void actualizarSubalternos(int id) {
 		sessionFactory.getCurrentSession().createQuery("update Empleado set jefe = NULL where jefe ="+id).executeUpdate();
 	}
+	public List<Empleado> listJefe() {
+		return (List<Empleado>) sessionFactory.getCurrentSession().createQuery("from Empleado where jefe = NULL").list();
+	}
 }
