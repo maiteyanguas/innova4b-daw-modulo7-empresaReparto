0f8652f8a76980e666c0af30c8701ee4fcc630a8
Aritz1216 <aritz1216@gmail.com>
2014-04-25 12:23:42 +0200

    Repositorio crear coche, controlador metodos GET/POST

--- src/main/java/innova4b/empresaReparto/coche/web/CocheController.java
+++ src/main/java/innova4b/empresaReparto/coche/web/CocheController.java
@@ -1,10 +1,15 @@
 package innova4b.empresaReparto.coche.web;
 
+import javax.validation.Valid;
+
+import innova4b.empresaReparto.coche.domain.Coche;
 import innova4b.empresaReparto.coche.repository.CocheDao;
+import innova4b.empresaReparto.empresa.domain.Empresa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,5 +30,18 @@ public class CocheController {
 	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
 	public void listAll(ModelMap model) {
 		model.addAttribute("coches", cocheDao.listAll());	
+	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public void newCoche(ModelMap model) {
+		model.addAttribute("coche",new Coche());
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(@Valid Coche coche, BindingResult result) {
+		if (result.hasErrors())
+			return "coche/new";
+		cocheDao.insert(coche);
+		return "redirect:/empresaReparto/coche/listAll";
 	}
 }
