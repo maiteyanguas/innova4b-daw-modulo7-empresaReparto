29412de21a3e62476d82c4d3f986e1470962aafa
xabiermartinez <xabi.ma@gmail.com>
2014-04-25 11:44:49 +0200

    Merge branch 'Stabilo_ListarEmpleados'
    Conflicts:
    	src/main/resources/scripts/import.sql

---
+++ src/main/java/innova4b/empresaReparto/empleado/service/EmpleadoService.java
@@ -0,0 +1,24 @@
+package innova4b.empresaReparto.empleado.service;
+
+import innova4b.empresaReparto.empleado.repository.EmpleadoDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmpleadoService {
+	
+	@Autowired
+	EmpleadoDao empleadoDao;
+
+	public void setEmpleadoDao(EmpleadoDao empleadoDao) {
+		this.empleadoDao = empleadoDao;
+	}
+
+	public void delete(int id){
+		if (empleadoDao.get(id).isJefe())
+			empleadoDao.actualizarSubalternos(id);
+		empleadoDao.delete(id);
+	}
+
+}
